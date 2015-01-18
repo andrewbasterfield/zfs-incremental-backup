93902faaaf40a683f1677eab118258b3fd27b7d6
Andrew Basterfield <abasterfield@gmail.com>
2015-01-18 22:30:54 +0000

    Provide support for doing the zfs recv on a different host * Don't terminte the shell pipe in the perl layer - delegate to the calling shell environment * In the shell code provide support for executing the zfs recv (and listing the snapshots) on a different server over SSH * General logic fixes (not related to the above) to ensure incomplete snapshots on the remote end are ignored

--- zfs-incremental.pl
+++ zfs-incremental.pl
@@ -5,9 +5,6 @@ use strict;
 use Data::Dumper;
 my $debug = 1;
 
-#my $src_dataset = "rdataset";
-#my $dst_dataset = "backup/rdataset";
-
 my $src_dataset = $ARGV[0];
 my $dst_dataset = $ARGV[1];
 
@@ -21,14 +18,16 @@ unless ($src_dataset && $dst_dataset) {
 #
 my $snaps = {};
 while (my $line = <STDIN>) {
-  if ($line =~ m/^$src_dataset@([0-9]{14})/) {
-    print STDERR "src: $1\n" if $debug;
-    $snaps->{$1}->{'src'} = 1;
+  if ($line =~ m/^$src_dataset(\/+.*)?@([0-9]{14})/) {
+    my $dataset = $1 || '';
+    print STDERR "src: $dataset $2\n" if $debug;
+    $snaps->{$dataset}->{$2}->{'src'} = 1;
   }
 
-  if ($line =~ m/^$dst_dataset@([0-9]{14})/) {
-    print STDERR "dst: $1\n" if $debug;
-    $snaps->{$1}->{'dst'} = 1;
+  if ($line =~ m/^$dst_dataset(\/+.*)?@([0-9]{14})/) {
+    my $dataset = $1 || '';
+    print STDERR "dst: $dataset $2\n" if $debug;
+    $snaps->{$dataset}->{$2}->{'dst'} = 1;
   }
 }
 
@@ -37,15 +36,47 @@ print STDERR Dumper($snaps) if $debug;
 #
 # Find the latest snap that is on both
 #
-my $start_snap;
-my @deletion_list;
-foreach my $snap (sort keys %$snaps) {
-  print STDERR "Verifying $snap\n" if $debug;
-  if ($snaps->{$snap}->{'src'} && $snaps->{$snap}->{'dst'}) {
-    $start_snap = $snap;
+my $candidate_snaps = {};
+foreach my $dataset (sort keys %$snaps) {
+  foreach my $snap (sort keys %{$snaps->{$dataset}}) {
+    $candidate_snaps->{$snap} = undef;
   }
-  push @deletion_list, $snap if $snaps->{$snap}->{'src'};
 }
+
+my @deletion_list;
+foreach my $dataset (sort keys %$snaps) {
+  foreach my $snap (sort keys %{$snaps->{$dataset}}) {
+    if (exists $candidate_snaps->{$snap}) {
+      print STDERR "Verifying $snap\n" if $debug;
+      if ($snaps->{$dataset}->{$snap}->{'src'} && $snaps->{$dataset}->{$snap}->{'dst'}) {
+        print STDERR "snapshot $snap is present on both source and destination datasets $dataset\n" if $debug;
+      } elsif (exists $snaps->{$dataset}->{$snap}->{'dst'}) {
+        print STDERR "$snap is not present on destination dataset $dataset, extended check to see if there are ANY snaps on src dataset\n" if $debug;
+        #
+        # If we have a candidate snapshot on the destination but there are no snapshots at all on the source that candidate snapshot is OK
+        #
+        foreach my $asnap (sort keys %{$snaps->{$dataset}}) {
+          if (exists $snaps->{$dataset}->{$asnap}->{'src'}) {
+            print STDERR "source dataset $dataset has snapshot $asnap, discounting snapshot $snap\n" if $debug;
+            delete $candidate_snaps->{$snap};
+            last;
+          }
+        }
+        print STDERR "snapshot $snap still acceptable; no snaps on src\n" if exists $candidate_snaps->{$snap} and $debug;
+      } else {
+        print STDERR "$snap is not present on destination of $dataset, discounting\n" if $debug;
+        delete $candidate_snaps->{$snap};
+      }
+    }
+    push @deletion_list, "$src_dataset$dataset\@$snap" if $snaps->{$dataset}->{$snap}->{'src'};
+  }
+}
+
+print STDERR Dumper($candidate_snaps) if $debug;
+
+my @candidate_list = sort keys %$candidate_snaps;
+
+my $start_snap = pop @candidate_list;
 
 print STDERR "Found starting snapshot $start_snap\n" if $start_snap && $debug;
 
@@ -59,15 +90,14 @@ my $end_snap = $src_dataset.'@'.$now;
 
 #print "\n";
 print "zfs snapshot -r $end_snap\n";
-print "zfs rollback -R $dst_dataset\@$start_snap\n" if defined $start_snap;
 #
 # -q quiet
 # -v verbosity zero
 # -s blocksize
 # -m total buffer size
 #
-print "zfs send -R ".(defined $start_snap ? "-i $start_snap" : "")." $end_snap | mbuffer -q -v 0 -s 128k -m 1024k | zfs recv $dst_dataset\n";
+print "zfs send -R ".(defined $start_snap ? "-i $start_snap" : "")." $end_snap\n";
 
 foreach my $snap (@deletion_list) {
-  print "zfs destroy -R $src_dataset\@$snap\n";
+  print "zfs destroy $snap\n";
 }
--- zfs-incremental.sh
+++ zfs-incremental.sh
@@ -2,4 +2,19 @@
 
 set -e
 
-zfs list -t snapshot | ./zfs-incremental.pl rpool backup/rpool | sh -e -x
+src_dataset="rpool"
+dst_dataset="zroot/backup"
+dst_user="backup@userver"
+
+
+if [ -n "$dst_user" ]; then
+  dst_ssh="ssh -c arcfour zfsbackup@userver sudo"
+fi
+
+#zfs list -t snapshot | ./zfs-incremental.pl "$src_dataset" "$dst_dataset" | sh -e -x | mbuffer -q -v 0 -s 128k -m 1024k | zfs recv "$dst_dataset"
+
+if [ -n "$dst_user" ]; then
+  dst_ssh="ssh -c arcfour zfsbackup@userver sudo"
+fi
+
+(zfs list -t snapshot; $dst_ssh zfs list -t snapshot) | ./zfs-incremental.pl "$src_dataset" "$dst_dataset" | sh -e -x | mbuffer -q -v 0 -s 128k -m 1024k | pv | $dst_ssh zfs recv -F "$dst_dataset"
